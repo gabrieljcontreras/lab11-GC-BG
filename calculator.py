4652dff5133e10d119c1770f82632611c9a1bf03
Brayden Gutowski <brayden@Braydens-MacBook-Pro-2.local>
2026-04-08 16:03:40 -0400

    combined calculator

--- calculator.py
+++ calculator.py
@@ -6,7 +6,13 @@ def add(a, b):
 
 # First example
 import math
-
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot have the square root of a negative number")
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    result = math.hypot(a,b)
+    return result
 def add(a, b):
     return a + b
 
@@ -17,11 +23,9 @@ def multiply(a, b):
     return a * b
 
 def logarithm(a, b):
-    try:
-        result = math.log(a, b)
-        return result
-    except ValueError:
-        print("Value Error")# use math library/raise ValueError
+    if a <= 0:
+        raise ValueError("Value Error")
+    return math.log(a, b)
 
 def exponent(a, b):
     return a**b
@@ -32,11 +36,9 @@ def mul(a, b):
     return mul
 
 def div(a, b):
-    try:
-        div = a / b
-        return div
-    except ZeroDivisionError:
-        print("Error: cannot divide by zero")
+    if a == 0:
+        raise ZeroDivisionError("ZeroDivisionError")
+    return b/a
 
 def log(a, b):
     try:
